ca7f6e8f770d6fa732eb05d05d208ef6817c3e00
unknown <juka.petra@gmail.com>
2020-06-01 22:23:26 +0200

    testiranje koda

--- test_dokumentarij.py
+++ test_dokumentarij.py
@@ -2,16 +2,15 @@ import unittest
 from dokumentarij import  DokumentarijController, DokumentarijModel, DokumentarijView
 
 class DokumentarijTestCase(unittest.TestCase):
+    def test_list(self):
+        game_test = DokumentarijController(DokumentarijModel(), DokumentarijView())
+        name_list = game_test.model.names
+        initial_list = len(name_list)
+        game_test.get_new_name()
+        after_list = len(name_list)
+        self.assertGreater(initial_list, after_list, "Broj studenata u dokumentariju  mora biti veći nakon dodavanja imena " )
+
    
-
-
-
-
-
-
-
-
-
 
 
 
